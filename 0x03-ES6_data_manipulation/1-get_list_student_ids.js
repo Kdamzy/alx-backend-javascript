c5559014bdff50d7a130022c90d1a976b871d7d8
Mikaelao <kolajidedamilare@gmail.com>
2024-08-13 20:52:09 +0100

    retrieve student

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,12 +1,12 @@
 /**
  * Create a function getListStudentIds that returns an array of ids from a list of object.
  * @param {{
-*   id: Number,
-*   firstName: String,
-*   location: String
-* }[]} studentList - The list of students.
-* @Kdamzy
-* @returns
+ * id: Number,
+ * firstName: String,
+ * location: String
+ * }[]} studentList - The list of students.
+ * @author Kehinde Olajide <https://github.com/Kdamzy>
+ * @returns
 */
 export default function getListStudentIds(studentList) {
  if (Array.isArray(studentList)) {
--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,13 +1,13 @@
 /**
  * Retrieves students in a given location.
  * @param {{
-*   id: Number,
-*   firstName: String,
-*   location: String
-* }[]} studentList - The list of students.
-* @param {String} city - The location.
-* @Kdamzy
-* @returns
+ * id: Number,
+ * firstName: String,
+ * location: String
+ * }[]} studentList - The list of students.
+ * @param {String} city - The location.
+ * @author Kehinde Olajide <https://github.com/Kdamzy>
+ * @returns
 */
 export default function getStudentsByLocation(studentList, city) {
  if (Array.isArray(studentList)) {
