fa354214243e4f2a8199200e2e0d8394c7c57158
Mikaelao <kolajidedamilare@gmail.com>
2024-08-13 20:58:08 +0100

    retrieve student

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,8 @@
 /**
  * Create a function getListStudentIds that returns an array of ids from a list of object.
- * @param {{
- * id: Number,
- * firstName: String,
- * location: String
- * }[]} studentList - The list of students.
- * @author Kehinde Olajide <https://github.com/Kdamzy>
- * @returns
-*/
+ * @Kdamzy
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ */
 export default function getListStudentIds(studentList) {
  if (Array.isArray(studentList)) {
    return studentList.map((student) => student.id);
